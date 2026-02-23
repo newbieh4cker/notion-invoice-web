ced44e8419e395ad0bb9be41753e51859813a23a
newbieh4cker <newbieh4cker@github.com>
2026-02-23 16:52:23 +0900

    refactor: InvoiceListContainer에서 useEffect 제거하고 props로 데이터 전달

--- components/invoice/invoice-list-container.tsx
+++ components/invoice/invoice-list-container.tsx
@@ -2,11 +2,11 @@
 
 /**
  * 견적서 목록 클라이언트 컨테이너 컴포넌트
- * Zustand 스토어 초기화 및 필터/검색/정렬/테이블 통합
- * 서버 컴포넌트에서 받은 더미 데이터를 스토어에 적재
+ * 서버에서 전달된 초기 데이터를 바탕으로 필터/검색/정렬/테이블 통합
+ * useEffect + Zustand 스토어 주입 방식 대신, props 직접 전달 방식 사용
+ * (Zustand는 UI 상태인 필터/검색/정렬 값만 관리)
  */
 
-import { useEffect } from "react"
 import { InvoiceFilters } from "@/components/invoice/invoice-filters"
 import { InvoiceSearch } from "@/components/invoice/invoice-search"
 import { InvoiceSort } from "@/components/invoice/invoice-sort"
@@ -20,26 +20,31 @@ interface InvoiceListContainerProps {
 }
 
 export function InvoiceListContainer({ initialInvoices }: InvoiceListContainerProps) {
-  const setInvoices = useInvoiceStore((s) => s.setInvoices)
-
-  // 컴포넌트 마운트 시 스토어에 초기 데이터 적재
-  useEffect(() => {
-    setInvoices(initialInvoices)
-  }, [initialInvoices, setInvoices])
+  // UI 상태(필터, 검색, 정렬)만 Zustand에서 구독
+  const filter = useInvoiceStore((s) => s.filter)
+  const searchTerm = useInvoiceStore((s) => s.searchTerm)
+  const sortBy = useInvoiceStore((s) => s.sortBy)
+  const sortOrder = useInvoiceStore((s) => s.sortOrder)
 
   return (
     <div className="space-y-4">
-      {/* 필터 탭 영역 */}
-      <InvoiceFilters />
+      {/* 필터 탭 - initialInvoices를 직접 전달하여 카운트 계산 */}
+      <InvoiceFilters invoices={initialInvoices} />
 
-      {/* 검색 및 정렬 컨트롤 영역 */}
+      {/* 검색 및 정렬 컨트롤 */}
       <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
         <InvoiceSearch />
         <InvoiceSort />
       </div>
 
-      {/* 견적서 테이블 */}
-      <InvoiceTable />
+      {/* 견적서 테이블 - 데이터와 UI 상태를 모두 props로 전달 */}
+      <InvoiceTable
+        invoices={initialInvoices}
+        filter={filter}
+        searchTerm={searchTerm}
+        sortBy={sortBy}
+        sortOrder={sortOrder}
+      />
     </div>
   )
 }
